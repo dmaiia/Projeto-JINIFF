d27368ff8ea7e7266777080fda68e181d2d159f4
Diego <dmaiia14@gmail.com>
2016-09-16 16:04:50 -0300

    Classes do pacote Entidade e Hibernate criadas

---
+++ src/main/java/Entidade/Modalidade.java
@@ -0,0 +1,80 @@
+
+package Entidade;
+
+import java.io.Serializable;
+import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Table;
+import javax.persistence.Entity;
+
+@Entity
+@Table(name = "modalidade")
+
+public class Modalidade implements Serializable {
+   @Id
+   @Column(name = "nome")
+   private String nome;
+   
+   @Column(name = "categoria")
+   private String categoria; 
+   
+   @Column(name = "classificacao")
+   private String classificacao; 
+   
+   @Column(name = "hora")
+   private String hora;
+   
+   @Column(name = "data")
+   private String data; 
+   
+   @Column(name = "local")
+   private String local; 
+   
+   
+
+   public Modalidade() {}
+   
+   public String getNome() {
+      return nome;
+   }
+   public void setNome(String nome) {
+      this.nome = nome;
+   }
+ 
+    public String getCategoria(){
+       return categoria;
+    }
+    
+   public void setCategoria(String categoria){
+      this.categoria = categoria;
+    }
+   
+   public String getClassificacao() {
+      return classificacao;
+   }
+   public void setClassificacao(String classificacao) {
+      this.classificacao = classificacao;
+   }
+   
+   public String getHora() {
+      return hora;
+   }
+   public void setHora(String hora) {
+      this.hora = hora;
+   }
+ 
+   public String getData() {
+      return data;
+   }
+   public void setData(String data) {
+      this.data = data;
+   }
+ 
+   public String getLocal() {
+      return local;
+   }
+   public void setLocal(String local) {
+      this.local = local;
+   }
+ 
+}
