d513f273ca6dab92936ef20a34c1f6327f7ce654
carnaval147 <wandinho2323@gmail.com>
2016-09-20 09:04:58 -0300

    Código Foda

--- src/main/java/Servlet/CadastroModalidade.java
+++ src/main/java/Servlet/CadastroModalidade.java
@@ -5,10 +5,110 @@
  */
 package Servlet;
 
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import Entidade.Modalidade;
+import Hibernate.ModalidadeDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+
 /**
  *
- * @author aluno
+ * @author Diego Maia
  */
-public class CadastroModalidade {
-    
+public class CadastroModalidade extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        PrintWriter out = response.getWriter();
+        try {
+            /* TODO output your page here. You may use following sample code. */
+            
+            String nome = request.getParameter("nome);
+            String categoria = request.getParameter("categoria");
+            String classificacao = request.getParameter("classificacao");
+            String hora = request.getParameter("hora");
+            String data = request.getParameter("data");
+            String local = request.getParameter("local");
+            
+            
+
+            Modalidade modalidade = new Modalidade();
+            
+            modalidade.setNome(nome);
+            modalidade.setCategoria(categoria);
+            modalidade.setClassificacao(classificacao);
+            modalidade.setHora(hora);
+            modalidade.setData(data );
+            modalidade.setLocal(local);
+            
+            
+            ModalidadeDAO moDAO = new ModalidadeDAO();
+            moDAO.addModalidade(modalidade);
+            response.sendRedirect("Modalidades.jsp");
+            
+        } finally {
+            out.close();
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
 }
