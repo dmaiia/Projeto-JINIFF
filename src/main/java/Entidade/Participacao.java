d918b340cbba9a9b82bc5d1dffbe8552ced8a0aa
Diego <dmaiia14@gmail.com>
2016-11-30 17:05:25 -0200

    Classe participação

--- src/main/java/Entidade/Participacao.java
+++ src/main/java/Entidade/Participacao.java
@@ -1,14 +1,52 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Entidade;
 
-/**
- *
- * @author aluno
- */
-public class Participacao {
+import java.io.Serializable;
+import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Table;
+import javax.persistence.Entity;
+
+@Entity
+@Table(name = "participacao")
+
+public class Participacao implements Serializable {
+   @Id
+   @Column(name = "lc_aluno")
+   private String lc_aluno;
+   
+   @Id
+   @Column(name = "lc_modalidade")
+   private String lc_modalidade;
+  
+   @Column(name = "data")
+   private String data; 
+   
+
+   
+   
+
+   public Participacao() {}
+   
+   public String getAluno() {
+      return lc_aluno;
+   }
+   public void setAluno(String lc_aluno) {
+      this.lc_aluno = lc_aluno;
+   }
+    public String getModalidade() {
+      return lc_modalidade;
+   }
+    public void setModalidade(String lc_modalidade) {
+      this.lc_modalidade = lc_modalidade;
+   }
+ 
+    public String getData(){
+       return data;
+    }
     
+   public void setData(String data){
+      this.data = data;
+    }
 }
+   
